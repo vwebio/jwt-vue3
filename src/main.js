0dcfd30d8bc9baf53c48b67604ffb6b2fd96edee
vmwebio <vmwebio@gmail.com>
2024-09-11 17:30:29 +0300

    update: обновил данные firebaseConfig

--- src/main.js
+++ src/main.js
@@ -1,35 +1,35 @@
-import 'primeicons/primeicons.css'
-import 'primeflex/primeflex.css'
-import PrimeVue from 'primevue/config'
-import Aura from '@primevue/themes/aura';
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { initializeApp } from "firebase/app"
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { initializeApp } from "firebase/app";
 
-
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCUYdawfZJ6kUdc2YGrjLtJesZ39Nn3Tbk",
-    authDomain: "jwt-firebase-vue3.firebaseapp.com",
-    projectId: "jwt-firebase-vue3",
-    storageBucket: "jwt-firebase-vue3.appspot.com",
-    messagingSenderId: "639394322703",
-    appId: "1:639394322703:web:92f6d932828b11f067c67a"
-  }
-  
-  initializeApp(firebaseConfig)
+  apiKey: "AIzaSyAS_BRgXKoue7kGxEIPc1XzV2idSmiKCnA",
+  authDomain: "vue3-jwt-pinia.firebaseapp.com",
+  databaseURL: "https://vue3-jwt-pinia-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "vue3-jwt-pinia",
+  storageBucket: "vue3-jwt-pinia.appspot.com",
+  messagingSenderId: "750520181105",
+  appId: "1:750520181105:web:5a8458ac3dd797b54296d0",
+};
 
-const app = createApp(App)
+initializeApp(firebaseConfig);
 
-app.use(createPinia())
-app.use(router)
-app.use(PrimeVue, {    
-    theme: {
-        preset: Aura,
-    }
- });
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+
+app.mount("#app");
